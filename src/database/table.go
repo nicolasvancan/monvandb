a679b8d958b861082db9963f9970c0a754f10091
nicolas montanha <nicolas@kunumi.com>
2024-06-16 10:52:53 -0300

    Chore: Started implementing basic functions for table operations

--- src/database/table.go
+++ src/database/table.go
@@ -1,41 +1,138 @@
 package database
 
+import (
+	btree "github.com/nicolasvancan/monvandb/src/btree"
+	utils "github.com/nicolasvancan/monvandb/src/utils"
+)
+
 /*
-Basic table interface package, which contains basic methodes for the table structure
+Table interface
+
+The table is the entry point to access the data stored within one Datafile (BTree) and holds also all methodes related to the file itself,
+whether it is just a simple get function, or even a range query, or scan. All those methodes are called within a function.
+
+The basic struct is composed of some table definition fields, such as the name of the table, the path to the table file, and the columns
+of the table. On the otherhand, the table also has other struct that are used to manipulate data or files.
+
+For this version of the database, the table struct is composed of the following fields:
+- Name: The name of the table
+- Path: The path to the table file
+- Columns: The columns of the table
+- PDataFile: A pointer to the DataFile struct, which is used to access the data stored in the table file
+
+### BASIC ACCESS METHODES ###
+
+- Get: Get one or more rows for a given key
+- Insert: Insert a row into the table
+- Update: Update a row in the table
+- Delete: Delete a row from the table
+- Range: Get all the rows in a given range
+
+### INDEX METHODES ###
+
+- CreateIndex: Create an index on a given column
+- DropIndex: Drop an index on a given column
+- GetIndex: Get the index on a given column
+- GetIndexValue: Get the value of the index on a given column
+- InsertIndexValue: Put the value of the index on a given column
+- UpdateIndexValue: Update the value of the index on a given column
+- DeleteIndexValue: Delete the value of the index on a given column
+
 */
 
-// Functions that are used to convert a row to column values and vice versa
-func (t *Table) MapRowToColumnValues(row RawRow) []ColumnValue {
-	// Create a map to hold the column values
-	columnValues := make([]ColumnValue, len(t.Columns))
+/*
+Get
 
-	// Loop through the columns and add them to the map
-	for index := range t.Columns {
-		// Get the value from the row
-		value := row[t.Columns[index].Name]
+Get one or more rows for a given key, returning the rows as a slice of RawRow.
 
-		// Create a new column value
-		columnValues[index] = ColumnValue{
-			Col:   uint16(index),
-			Value: value,
-		}
+This method is now really simple, but later, depending on the implementation of the database, it could be more complex.
+For instance, let's say that we have a table with a primary key, and we want to get the row with the primary key. In this case,
+the function uses one *PDataFile to access the data.
+
+When running for indexed columns,
+*/
+func (t *Table) Get(column string, value any) ([]RawRow, error) {
+	/*
+		Reserved for further implementation, evaluate if the column is indexed or not, and then load the appropriate
+		binary tree to get the data. Otherwise, use the default which is the datafile.
+	*/
+	row := make([]RawRow, 0)
+	// Get the pk column
+	pk := t.PrimaryKey
+	serializedValue, err := utils.Serialize(value)
+
+	if err != nil {
+		return nil, err
 	}
 
-	return columnValues
+	// If the column is the primary key, we can use the datafile to get the data
+	if pk.Name == column {
+		// Serialize the value
+
+		row = append(row, t.FromKeyValueToRawRow(t.PDataFile.Get(serializedValue))...)
+		return row, nil
+	}
+
+	// We try to find indexed DataFiles
+	index, ok := t.Indexes[column]
+	if ok {
+		// Get the index DataFile
+		indexDataFile := index.PDataFile
+		// Get the value from the index
+		indexValue := indexDataFile.Get(serializedValue)
+		row = append(row, t.FromKeyValueToRawRow(indexValue)...)
+	} else {
+		// Worst case, there must be a scan through the datafile
+		// Create a crawler at the beginning of the file
+		crawler := btree.GoToFirstLeaf(t.PDataFile.GetBTree())
+		// Loop through the datafile
+		keyValues := make([]btree.BTreeKeyValue, 0)
+
+		for {
+			kv := crawler.GetKeyValue()
+			keyValues = append(keyValues, kv)
+
+			// If the crawler is at the end of the file, break the loop
+			if err = crawler.Next(); err != nil {
+				break
+			}
+		}
+		row = append(row, t.FromKeyValueToRawRow(keyValues)...)
+	}
+
+	return row, nil
 }
 
-func (t *Table) FromColumnValuesToRow(columnValues []ColumnValue) RawRow {
-	// Create a map to hold the column values
-	row := make(RawRow)
+/*
+Insert a row into the table for given []RawRow.
+*/
 
-	// Loop through the columns and add them to the map
-	for index := range t.Columns {
-		// Get the column value
-		columnValue := columnValues[index]
+func (t *Table) Insert([]RawRow) int {
+	return 0
+}
 
-		// Add the value to the row
-		row[t.Columns[index].Name] = columnValue.Value
+/*
+Update a row into the table for given []RawRow
+*/
+
+func (t *Table) Update(RawRow) int {
+	return 0
+}
+
+func (t *Table) Delete([]RawRow) int {
+	return 0
+}
+
+func (t *Table) isColumnIndexed(column string) bool {
+	_, ok := t.Indexes[column]
+	return ok
+}
+
+func (t *Table) GetColumnByName(colName string) *Column {
+	for _, col := range t.Columns {
+		if col.Name == colName {
+			return &col
+		}
 	}
-
-	return row
+	return nil
 }
--- src/database/types.go
+++ src/database/types.go
@@ -17,8 +17,6 @@ const (
 	COL_TYPE_BLOB
 )
 
-type RawRow = map[string]interface{}
-
 type Database struct {
 	Name       string            // Database's name
 	Tables     map[string]*Table // reference to Tables
@@ -27,10 +25,91 @@ type Database struct {
 }
 
 type Table struct {
-	Name      string          // Table's name
-	Path      string          // Where the table configuration is stored
-	Columns   []Column        // reference to Columns
-	PDataFile *files.DataFile // private Access btree (Simple)
+	Name         string           // Table's name
+	Path         string           // Where the table configuration is stored
+	Columns      []Column         // reference to Columns
+	PrimaryKey   Column           // reference to PrimaryKey
+	CompositeKey []Column         // Case column is composite
+	Indexes      map[string]Index // reference to Indexes
+	PDataFile    *files.DataFile  // private Access btree (Simple)
+}
+
+type RawRow = map[string]interface{}
+
+const (
+	ASC = iota
+	DESC
+)
+
+const (
+	EQ = iota
+	GT
+	GTE
+	LT
+	LTE
+	NE
+)
+
+type RangeOptions struct {
+	From        []byte
+	To          []byte
+	FComparator int             // From Comparator, indicates what type of comparation should be done with the From value ex: GTE or GT
+	TComparator int             // To Comparator, indicates what type of comparation should be done with the To value
+	Order       int             // Order of the range wheter is ASC os DESC
+	Limit       int             // Limit of the range
+	PDataFile   *files.DataFile // Pointer to the data file to be used
+}
+
+/*
+TableQueryOperation is a struct that represents the literal query conditions that can be used to query a table.
+One exameple is the following WHERE statement:
+
+WHERE id > 10 AND (name = 'John' OR age < 30)
+
+This statement can be represented as the following TableQueryOperation struct:
+
+	TableQueryOperation{
+		Column: "id",
+		Comparator: GT,
+		Value: 10,
+		And: []TableQueryOperation{
+			TableQueryOperation{
+				Column: "name",
+				Comparator: EQ,
+				Value: "John",
+				And: nil,
+				Or: []TableQueryOperation{
+					TableQueryOperation{
+						Column: "age",
+						Comparator: LT,
+						Value: 30,
+						And: nil,
+						Or: nil,
+					}
+				}
+			},
+		},
+
+		Or: nil,
+	}
+
+This structure is also used to represent the conditions of a range query, for example:
+Knowing that the id is the primary key of the table, we know that the primary key is indexed and the value must be greater than 10
+Therefore, it doesn't matter the other columns conditions, the range will not be altered because of them.
+*/
+type TableQueryOperation struct {
+	Column     string
+	Comparator int
+	Value      interface{}
+	And        []TableQueryOperation
+	Or         []TableQueryOperation
+}
+
+type Index struct {
+	Name      string
+	Column    string
+	Path      string
+	PDataFile *files.DataFile
 }
 
 type ColumnValue struct {
@@ -52,5 +131,4 @@ type Column struct {
 	AutoIncrement bool
 	Primary       bool
 	Unique        bool
-	Foreign       bool
 }
